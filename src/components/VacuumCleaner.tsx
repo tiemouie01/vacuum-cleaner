fb3ec159e32c7d0144a45dcacc1ea2a1101fa956
Timothy Miamba <timal0361@gmail.com>
2025-02-13 17:48:17 +0200

    Update vacuum cleaner design

--- src/components/VacuumCleaner.tsx
+++ src/components/VacuumCleaner.tsx
@@ -2,7 +2,11 @@ export default function VacuumCleaner() {
   return (
     <div>
       <div className="w-16 h-16 bg-gray-700 rounded-full flex items-center justify-center">
-        <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
+        <div className="w-8 h-8 transform rotate-180">
+          <svg viewBox="0 0 24 24" className="fill-gray-300">
+            <path d="M12 2L24 22H0L12 2Z" />
+          </svg>
+        </div>
       </div>
     </div>
   );
